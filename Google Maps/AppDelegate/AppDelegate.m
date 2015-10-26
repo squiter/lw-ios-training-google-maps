5536201782787481387724a15e9df9293ecb3689
Brunno dos Santos <squiter85@gmail.com>
2015-10-26 10:35:28 -0200

    Add Google API Key

--- Google Maps/AppDelegate/AppDelegate.m
+++ Google Maps/AppDelegate/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [GMSServices provideAPIKey:@"AIzaSyBlQS7jTBUpQnKcnN0ZKxCekxesyR-a1Tg"];
+    
     return YES;
 }
 
